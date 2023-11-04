dca502d48ac4bfe966da67e7161fb5edd63f30d3
Hitoshi Hoshiyama <5512994+HitoshiHoshiyama@users.noreply.github.com>
2023-11-04 15:50:06 +0900

    Add: ExplorerSingleModeプロジェクトを追加

---
+++ ExplorerSingleMode/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.InteropServices;
+using System.Windows.Automation;
+
+using ExplorerSingleMode;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var winElmMap = new Dictionary<IntPtr, AutomationElement>();
+        var tabNumMap = new Dictionary<IntPtr, int>();
+
+        var shellType = Type.GetTypeFromProgID("Shell.Application");
+        if (shellType is null) return;
+        dynamic shellInstance = Activator.CreateInstance(shellType);
+        try
+        {
+            var shellWindows = shellInstance.Windows();
+            var comList = new List<dynamic>();
+            // ExplorerのCOMオブジェクトリスト作成
+            for (int idx = 0; idx < shellWindows.Count; idx++)
+            {
+                var shellItem = shellWindows.Item(idx);
+                if (shellItem == null) continue;
+                var path = System.IO.Path.GetFileName((string)shellItem.FullName);
+                if (path.ToLower() == "explorer.exe" && shellItem.Document.FocusedItem is not null) comList.Add(shellItem);
+            }
+            // タブ部分のHWNDとAutomationElementマップ作成
+            foreach (var comObj in comList)
+            {
+                if (winElmMap.ContainsKey((IntPtr)comObj.Hwnd)) { continue; }
+                var ExplorerInfo = ExplorerSingleMode.WindowManager.GetExprolerInfo((IntPtr)comObj.Hwnd);
+                if (ExplorerInfo is null) continue;
+                winElmMap.Add((IntPtr)comObj.Hwnd, ExplorerInfo.Item1);
+                tabNumMap.Add((IntPtr)comObj.Hwnd, ExplorerInfo.Item2);
+            }
+        }
+        finally
+        {
+            Marshal.FinalReleaseComObject(shellInstance);
+        }
+
+        // 母艦にするウィンドウを決定する
+        IntPtr baseHwd = IntPtr.Zero;
+        int tabMax = 0;
+        foreach(var item in tabNumMap.ToList())
+        {
+            if (tabMax < item.Value)
+            {
+                tabMax = item.Value;
+                baseHwd = item.Key;
+            }
+        }
+        AutomationElement tgt = null;
+        if (baseHwd != IntPtr.Zero)
+        {
+            // 母艦とその他を分離
+            tgt = winElmMap[baseHwd];
+            winElmMap.Remove((IntPtr)baseHwd);
+
+            // 現存する全ウィンドウをマージ
+            foreach (var item in winElmMap.ToList())
+            {
+                // タブ個数の分だけ繰り返し
+                for (int idx = 0; idx < tabNumMap[item.Key]; idx++)
+                {
+                    var src = item.Value;
+                    if (idx > 0)
+                    {
+                        // タブが減るとHWDが振り直されるため再取得
+                        src = ExplorerSingleMode.WindowManager.GetExprolerInfo(item.Key, false).Item1;
+                    }
+                    ExplorerSingleMode.WindowManager.DragExplorerTab(src, tgt);
+                }
+            }
+        }
+        winElmMap.Clear();
+        tabNumMap.Clear();
+
+        Automation.AddAutomationEventHandler(
+            WindowPattern.WindowOpenedEvent,
+            AutomationElement.RootElement,
+            TreeScope.Subtree,
+            (sender, e) =>
+            {
+                if (e.EventId == WindowPattern.WindowOpenedEvent)
+                {
+                    AutomationElement element = sender as AutomationElement;
+                    // TODO: コントロールパネルの除外
+                    if (element.Current.ClassName == "CabinetWClass")
+                    {
+                        Console.WriteLine("エクスプローラーウィンドウ({0:x})が開かれました", element.Current.NativeWindowHandle);
+                        EventQueue.Add((IntPtr)element.Current.NativeWindowHandle);
+                    }
+                }
+            });
+
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(OnCanceled);
+        while (true)
+        {
+            try
+            {
+                IntPtr hwnd = EventQueue.Take(Cancel.Token);
+                var ExplorerInf = ExplorerSingleMode.WindowManager.GetExprolerInfo(hwnd, false);
+                if (ExplorerInf is not null) ExplorerSingleMode.WindowManager.DragExplorerTab(ExplorerInf.Item1, tgt);
+            }
+            catch(OperationCanceledException)
+            {
+                Console.WriteLine("teminate request accepted.");
+                break;
+            }
+            catch (NoTargetException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                tgt = AutomationElement.FromHandle(ex.Hwnd);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        Console.WriteLine("teminated.");
+        Cancel.Dispose();
+        EventQueue.Dispose();
+    }
+
+    private static void OnCanceled(object sender, ConsoleCancelEventArgs e)
+    {
+        Console.WriteLine("teminate requested.");
+        e.Cancel = true;
+        Cancel.Cancel();
+    }
+
+    private static AutomationElementCollection FindElements(AutomationElement rootElement, string automationClass)
+    {
+        return rootElement.FindAll(
+            TreeScope.Subtree,
+            new PropertyCondition(
+              AutomationElement.ClassNameProperty,
+              automationClass));
+    }
+
+    private static BlockingCollection<IntPtr> EventQueue = new BlockingCollection<IntPtr>();
+    private static CancellationTokenSource Cancel = new CancellationTokenSource();
+}
---
+++ ExplorerSingleMode/WindowManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace ExplorerSingleMode
+{
+    static internal class WindowManager
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void SetCursorPos(int X, int Y);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetSystemMetrics(int smIndex);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool IsIconic(IntPtr hWnd);
+
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        private const int MOUSEEVENTF_MOVE = 0x1;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x2;
+        private const int MOUSEEVENTF_LEFTUP = 0x4;
+
+        private const int SM_CXSCREEN = 0;
+        private const int SM_CYSCREEN = 1;
+
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_MINIMIZE = 6;
+
+        public static Tuple<AutomationElement, int> GetExprolerInfo(IntPtr Hwnd, bool NeedTabCount = true)
+        {
+            var WinElm = AutomationElement.FromHandle(Hwnd);
+            var TitleElm = FindElements(WinElm, "TITLE_BAR_SCAFFOLDING_WINDOW_CLASS");
+            if (TitleElm is null || TitleElm.Count == 0) return null;
+            if (NeedTabCount) ShowWindow(Hwnd, SW_SHOWNORMAL);          // 省くと最小化されたウィンドウのタブが0とカウントされる
+            var TabNum = NeedTabCount ? FindElements(WinElm, "ShellTabWindowClass").Count : 1;
+            if (TabNum == 0) return null;
+            return new Tuple<AutomationElement, int>(TitleElm[0], TabNum);
+        }
+
+        public static void DragExplorerTab(AutomationElement Src, AutomationElement Target)
+        {
+            if (Target is null) throw new NoTargetException((IntPtr)Src.Current.NativeWindowHandle);
+            var IsMin = IsIconic((IntPtr)Target.Current.NativeWindowHandle);
+            if (IsMin) ShowWindow((IntPtr)Target.Current.NativeWindowHandle, SW_SHOWNORMAL);
+            if(!Target.Current.IsEnabled)throw new NoTargetException((IntPtr)Src.Current.NativeWindowHandle);
+            var TgtRect = Target.Current.BoundingRectangle;
+            var TgtScreen = Screen.FromHandle((IntPtr)Target.Current.NativeWindowHandle);
+            var TgtPosCorrect = new Point(TgtScreen.WorkingArea.Left - TgtScreen.Bounds.X, TgtScreen.WorkingArea.Top - TgtScreen.Bounds.Y);
+
+            var SrcRect = Src.Current.BoundingRectangle;
+            var SrcScreen = Screen.FromHandle((IntPtr)Src.Current.NativeWindowHandle);
+            var SrcPosCorrect = new Point(SrcScreen.WorkingArea.Left - SrcScreen.Bounds.X, SrcScreen.WorkingArea.Top - SrcScreen.Bounds.Y);
+            var x = (int)SrcRect.X - SrcPosCorrect.X;
+            var y = (int)SrcRect.Y - SrcPosCorrect.Y + 20;
+            SetCursorPos(x, y);
+            SetForegroundWindow((IntPtr)Src.Current.NativeWindowHandle);
+            System.Threading.Thread.Sleep(50);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            System.Threading.Thread.Sleep(50);
+            mouse_event(MOUSEEVENTF_MOVE, 10, 0, 0, 0);
+            System.Threading.Thread.Sleep(100);
+            SetForegroundWindow((IntPtr)Target.Current.NativeWindowHandle);
+            var smx = GetSystemMetrics(SM_CXSCREEN);
+            var smy = GetSystemMetrics(SM_CYSCREEN);
+            x = ((int)TgtRect.Right - TgtPosCorrect.X) * (65535 / smx);
+            y = ((int)TgtRect.Y - TgtPosCorrect.Y + 30) * (65535 / smy);
+            mouse_event(MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE, x, y, 0, 0);
+            System.Threading.Thread.Sleep(150);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            System.Threading.Thread.Sleep(900);
+            if (IsMin) ShowWindow((IntPtr)Target.Current.NativeWindowHandle, SW_MINIMIZE);
+        }
+
+        private static AutomationElementCollection FindElements(AutomationElement rootElement, string automationClass)
+        {
+            return rootElement.FindAll(TreeScope.Subtree, new PropertyCondition(AutomationElement.ClassNameProperty, automationClass));
+        }
+    }
+
+    internal class NoTargetException : Exception
+    {
+        public NoTargetException(IntPtr Hwnd) { this.Hwnd = Hwnd; }
+        public IntPtr Hwnd { get; private set; }
+    }
+}
