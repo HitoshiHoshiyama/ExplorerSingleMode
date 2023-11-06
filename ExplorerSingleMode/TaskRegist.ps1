d50e3677e9a689e776727e98dc527eb995dcd591
Hitoshi Hoshiyama <5512994+HitoshiHoshiyama@users.noreply.github.com>
2023-11-06 23:57:15 +0900

    Add: タスク削除スクリプト Mod: 登録済みタスクを削除する前に停止するよう変更

--- ExplorerSingleMode/TaskRegist.ps1
+++ ExplorerSingleMode/TaskRegist.ps1
@@ -16,12 +16,16 @@ Write-Host タスク:ExplorerSingleMode の実行ファイル[$ExePath]
 $IsExist = Get-ScheduledTask | Select-String ExplorerSingleMode
 if(-not [string]::IsNullOrEmpty($IsExist))
 {
-    write-Host タスク:ExplorerSingleMode が既に存在するので削除します。
-    Unregister-ScheduledTask -TaskName "ExplorerSingleMode" -Confirm:$false
+    . .\TaskRemove.ps1 -NoWait
 }
 
 Register-ScheduledTask -TaskName "ExplorerSingleMode" -Action $TaskAction -Trigger $TaskTrigger -Settings $TaskOptions -RunLevel "Highest" -Description "エクスプローラのタブをひとつのウィンドウにまとめます。"
 Write-Host タスク:ExplorerSingleMode を登録しました。
+
+Start-ScheduledTask -TaskName "ExplorerSingleMode"
+Write-Host タスク:ExplorerSingleMode を開始しました。
+
+Get-ScheduledTask -TaskName "ExplorerSingleMode"
 
 $KeyIn = Read-Host "何かキーを押すと終了します。"
 Write-Host $KeyIn
---
+++ ExplorerSingleMode/TaskRemove.ps1
@@ -0,0 +1,22 @@
+﻿Param([Parameter(Mandatory=$false)][switch]$NoWait)
+
+$IsExist = Get-ScheduledTask | Select-String ExplorerSingleMode
+if(-not [string]::IsNullOrEmpty($IsExist))
+{
+    $Task = Get-ScheduledTask -TaskName ExplorerSingleMode
+    if(($Task).State -eq 'Running')
+    {
+        write-Host タスク:ExplorerSingleMode を停止します。
+        Stop-ScheduledTask -TaskName "ExplorerSingleMode"
+    }
+    write-Host タスク:ExplorerSingleMode を削除します。
+    Unregister-ScheduledTask -TaskName "ExplorerSingleMode" -Confirm:$false
+}else {
+    write-Host タスク:ExplorerSingleMode は登録されていません。
+}
+
+if(-not $NoWait)
+{
+    $KeyIn = Read-Host "何かキーを押すと終了します。"
+    Write-Host $KeyIn
+}
